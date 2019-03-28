2f9a8f533dee25cf94f7128c23817688570e1129
Jay Hickey <jayhickey@fb.com>
2019-03-28 10:12:48 -0700

    Change Swift Package Manager product from executible to library

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "ReSwift-Router",
     products: [
-      .executable(name: "ReSwift-Router", targets: ["ReSwiftRouter"]),
+      .library(name: "ReSwift-Router", targets: ["ReSwiftRouter"]),
     ],
     dependencies: [
       .package(url: "https://github.com/ReSwift/ReSwift", .upToNextMajor(from: "4.0.1"))
