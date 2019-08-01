c3c8c3156c4c911dfa7961319d1772da76bf6602
Henrik Lineholm <henrik.lineholm@icloud.com>
2019-08-01 09:38:42 +0200

    Reline Routable Protocol

--- ReSwiftRouter/Routable.swift
+++ ReSwiftRouter/Routable.swift
@@ -10,46 +10,26 @@ public typealias RoutingCompletion = () -> Void
 
 public protocol Routable {
 
-    func push(
-        _ element: RouteElement,
-        animated: Bool,
-        completion: @escaping RoutingCompletion) -> Routable
+    func push(_ element: RouteElement, animated: Bool, completion: @escaping RoutingCompletion) -> Routable
 
-    func pop(
-        _ element: RouteElement,
-        animated: Bool,
-        completion: @escaping RoutingCompletion)
+    func pop(_ element: RouteElement, animated: Bool, completion: @escaping RoutingCompletion)
 
-    func change(
-        _ from: RouteElement,
-        to: RouteElement,
-        animated: Bool,
-        completion: @escaping RoutingCompletion) -> Routable
+    func change(_ from: RouteElement, to: RouteElement, animated: Bool, completion: @escaping RoutingCompletion) -> Routable
 
 }
 
 extension Routable {
 
-    public func push(
-        _ element: RouteElement,
-        animated: Bool,
-        completion: @escaping RoutingCompletion) -> Routable {
-            fatalError("This routable cannot push elements. You have not implemented it. (Asked \(type(of: self)) to push \(element))")
+    public func push(_ element: RouteElement, animated: Bool, completion: @escaping RoutingCompletion) -> Routable {
+        fatalError("This routable cannot push elements. You have not implemented it. (Asked \(type(of: self)) to push \(element))")
     }
 
-    public func pop(
-        _ element: RouteElement,
-        animated: Bool,
-        completion: @escaping RoutingCompletion) {
-            fatalError("This routable cannot pop elements. You have not implemented it. (Asked \(type(of: self)) to pop \(element))")
+    public func pop(_ element: RouteElement, animated: Bool, completion: @escaping RoutingCompletion) {
+        fatalError("This routable cannot pop elements. You have not implemented it. (Asked \(type(of: self)) to pop \(element))")
     }
 
-    public func change(
-        _ from: RouteElement,
-        to: RouteElement,
-        animated: Bool,
-        completion: @escaping RoutingCompletion) -> Routable {
-            fatalError("This routable cannot change elements. You have not implemented it. (Asked \(type(of: self)) to change from \(from) to \(to))")
+    public func change(_ from: RouteElement, to: RouteElement, animated: Bool, completion: @escaping RoutingCompletion) -> Routable {
+        fatalError("This routable cannot change elements. You have not implemented it. (Asked \(type(of: self)) to change from \(from) to \(to))")
     }
 
 }
